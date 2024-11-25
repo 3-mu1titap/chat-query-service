6c30080a6321105c81baaea984fbd2d716ef5f29
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-11-25 09:17:54 +0900

    first commit

---
+++ src/main/java/com/multitap/chatQuery/ChatQueryApplication.java
@@ -0,0 +1,13 @@
+package com.multitap.chatQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatQueryApplication.class, args);
+	}
+
+}
