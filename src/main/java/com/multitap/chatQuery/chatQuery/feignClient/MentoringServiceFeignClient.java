70911f547805602667d15275e7e89c7f6f9c80e8
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-12-04 16:23:13 +0900

    Feat: 페인클라이언트 url 수정 #14

--- src/main/java/com/multitap/chatQuery/chatQuery/feignClient/MentoringServiceFeignClient.java
+++ src/main/java/com/multitap/chatQuery/chatQuery/feignClient/MentoringServiceFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:9001", name = "mentoring-query-service")
+@FeignClient(url = "http://api.adaptors.site", name = "mentoring-query-service")
 public interface MentoringServiceFeignClient {
 
     // mentoring-query-service 의 세션 조회 api 호출
