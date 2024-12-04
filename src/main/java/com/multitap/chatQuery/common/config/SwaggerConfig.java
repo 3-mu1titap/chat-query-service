045d392cf3f67cb21c2bb732a27d741e94444704
Oudaegwan <163433558+daegwan00@users.noreply.github.com>
2024-12-04 16:46:29 +0900

    Update SwaggerConfig.java
    ci-test

--- src/main/java/com/multitap/chatQuery/common/config/SwaggerConfig.java
+++ src/main/java/com/multitap/chatQuery/common/config/SwaggerConfig.java
@@ -41,5 +41,6 @@ public class SwaggerConfig {
                 .title("CHAT QUERY SERVICE")
                 .description("CHAT QUERY SERVICE Swagger UI")
                 .version("1.0.0");
+        //
     }
 }
