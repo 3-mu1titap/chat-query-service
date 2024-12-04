db05e8d24cfa9a609332a808528b406ddc70cd9c
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-12-04 18:03:13 +0900

    Infra

--- src/main/java/com/multitap/chatQuery/chatQuery/feignClient/MentoringServiceFeignClient.java
+++ src/main/java/com/multitap/chatQuery/chatQuery/feignClient/MentoringServiceFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://api.adaptors.site/mentoring-query-service", name = "mentoring-query-service")
+@FeignClient(name = "mentoring-query-service")
 public interface MentoringServiceFeignClient {
 
     // mentoring-query-service 의 세션 조회 api 호출
